b2b9aea6b49ea5d66198025ff43def2ba00ab23d
Horu97 <78439767+Horu97@users.noreply.github.com>
2021-02-15 16:51:08 +0100

    fatto parte Davide

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
-var data = [];
-document.getElementById("loginBtn").addEventListener("click", displayDate);
+var userId = document.getElementById('userId');
+var userPass = document.getElementById('userPass');
+var loginBtn = document.getElementById('loginBtn');
+var logoutBtn = document.getElementById('logoutBtn');
+var contactName = document.getElementById('contactName');
+var contactMail = document.getElementById('contactMail');
+var contactTel = document.getElementById('contactTel');
+var newUserBtn = document.getElementById('newUserBtn');
+
+var contatti = [];
+if (localStorage.getItem('contatti') != null) {
+    contatti = JSON.parse(localStorage.getItem('contatti'));
+}
+
+loginBtn.addEventListener('click', login, false);
+logoutBtn.addEventListener('click', logout, false);
+newUserBtn.addEventListener('click', addContact, false);
+
 function login(){
 
 }
+
 function logout(){
 
 }
+
 function addContact(){
+    if (contactName.value.trim() != "" && contactMail.value.trim() != "" && contactTel.value.trim() != "") {
+        var newContatto = {
+            "name": contactName.value,
+            "email": contactMail.value,
+            "tel": contactTel.value
+        };
+        contatti.push(newContatto);
+
+        // console.log(JSON.stringify(newContatto));
+        localStorage.setItem('contatti', JSON.stringify(contatti));
+    } else {
+
+    }
 }
+
 function renderContacts(){
+
 }
+
 function enableEditing(){
+    
 }
