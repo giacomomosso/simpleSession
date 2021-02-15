1a552e9150097db2afe1db3c7e6b1d0e6ed5f13b
giacomo mosso <giacomomosso@gmail.com>
2021-02-15 14:57:33 +0100

    u

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var data = [];
+document.getElementById("loginBtn").addEventListener("click", displayDate);
+function login(){
+
+}
+function logout(){
+
+}
+function addContact(){
+}
+function renderContacts(){
+}
+function enableEditing(){
+}
