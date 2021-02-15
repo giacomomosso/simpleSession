63a985934e1b8f458db8d13e853bcd6d825e4853
giacomo mosso <giacomomosso@gmail.com>
2021-02-15 16:36:55 +0100

    u

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 var data = [];
-document.getElementById("loginBtn").addEventListener("click", displayDate);
-function login(){
 
+document.getElementById("loginBtn").addEventListener("click", displayDate);
+
+function login(){
+    if (Modernizr.localstorage) {
+        var user = document.getElementById('userId');
+        var password = document.getElementById('userPass');
+    
+        if(user=="admin"&&password=="admin"){
+            localStorage.setItem('session',"admin");
+        }
+    
+        if(user=="user"&&password=="user"){
+            localStorage.setItem('session', "user" );
+        }
+    }
 }
 function logout(){
-
+     
 }
 function addContact(){
 }
@@ -12,3 +25,24 @@ function renderContacts(){
 }
 function enableEditing(){
 }
+
+
+
+//TEST SUPPORTO
+function isSupportato() {
+    try {
+        localStorage.setItem('test', 'Hello Local');
+        sessionStorage.setItem('loggedIn', true);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+if (isSupportato()) {
+    console.log("I local e session sono supportati");
+}
+
+//Controllo su utente loggato
+// sessionStorage.removeItem
+(sessionStorage.loggedIn) ? console.log("Sei loggato") : console.log("Sei offline");;
