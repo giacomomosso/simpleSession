22bd81d9ed5f063306f13de3169b284b278a5ab9
Horu97 <78439767+Horu97@users.noreply.github.com>
2021-02-15 17:38:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -69,24 +69,3 @@ function renderContacts(){
 
 function enableEditing(){
 }
-
-
-
-//TEST SUPPORTO
-function isSupportato() {
-    try {
-        localStorage.setItem('test', 'Hello Local');
-        sessionStorage.setItem('loggedIn', true);
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
-
-if (isSupportato()) {
-    console.log("I local e session sono supportati");
-}
-
-//Controllo su utente loggato
-// sessionStorage.removeItem
-// (sessionStorage.loggedIn) ? console.log("Sei loggato") : console.log("Sei offline");
