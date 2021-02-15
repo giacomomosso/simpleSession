86d1df6942bee4cff50d3252542243bdabb0bc3f
Ayoub-Bettaieb-Peano-5C <layardx90@gmail.com>
2021-02-15 17:32:55 +0100

    u

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-var data = [];
+var userId = document.getElementById('userId');
+var userPass = document.getElementById('userPass');
+var loginBtn = document.getElementById('loginBtn');
+var logoutBtn = document.getElementById('logoutBtn');
+var contactName = document.getElementById('contactName');
+var contactMail = document.getElementById('contactMail');
+var contactTel = document.getElementById('contactTel');
+var newUserBtn = document.getElementById('newUserBtn');
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
 
-document.getElementById("loginBtn").addEventListener("click", displayDate);
+var contatti = [];
+if (localStorage.getItem('contatti') != null) {
+    contatti = JSON.parse(localStorage.getItem('contatti'));
+}
+
+loginBtn.addEventListener('click', login, false);
+logoutBtn.addEventListener('click', logout, false);
+newUserBtn.addEventListener('click', addContact, false);
 
 function login(){
     if (Modernizr.localstorage) {
@@ -17,32 +33,44 @@ function login(){
     }
 }
 function logout(){
+
+    if (session in localStorage) {
+        try {
+            localStorage.removeItem(session); 
+            
+        } catch (error) {
+            console.log("session error 2");
+        }
+    } else {
+        console.log("session error 2");
+    } 
      
 }
 function addContact(){
+    if (contactName.value.trim() != "" && contactMail.value.trim() != "" && contactTel.value.trim() != "") {
+        var newContatto = {
+            "name": contactName.value,
+            "email": contactMail.value,
+            "tel": contactTel.value
+        };
+        contatti.push(newContatto);
+
+        // console.log(JSON.stringify(newContatto));
+        localStorage.setItem('contatti', JSON.stringify(contatti));
+    } else {
+
+    }
 }
 function renderContacts(){
 }
 function enableEditing(){
-}
+    if(Modernizr.localstorage){
+        if(localStorage.getItem("session") == "admin"){
 
-
-
-//TEST SUPPORTO
-function isSupportato() {
-    try {
-        localStorage.setItem('test', 'Hello Local');
-        sessionStorage.setItem('loggedIn', true);
-        return true;
-    } catch (error) {
-        return false;
+            
+            form1.hidden = true;
+            form2.hidden = false;
+        }
     }
 }
 
-if (isSupportato()) {
-    console.log("I local e session sono supportati");
-}
-
-//Controllo su utente loggato
-// sessionStorage.removeItem
-(sessionStorage.loggedIn) ? console.log("Sei loggato") : console.log("Sei offline");;
